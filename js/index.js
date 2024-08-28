ea90f5eababa7243275bc56df824816f24115f50
Irina Khameeva <irina.khameeva@gmail.com>
2024-08-27 23:11:39 -0400

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -17,3 +17,40 @@ for (let i = 0; i < skills.length; i++) {
     skillLi.innerHTML = skills[i];
     skillsList.appendChild(skillLi);
 }
+
+//Handle Message Form Submit
+
+const messageForm = document.querySelector("form[name='leave_message']");
+messageForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = e.target.usersName.value;
+    const email = e.target.usersEmail.value;
+    const messages = e.target.usersMessage.value;
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Message:", messages);
+
+    //Display Messages in List
+
+    const messageSection = document.getElementById("messages");
+    const messageList = messageSection.querySelector("ul");
+    const newMessage = document.createElement("li");
+    newMessage.innerHTML = `<a href = "mailto:${email}"> ${name} </a> 
+                    <span>wrote: ${messages}</span>`;
+
+    //Create a variable named removeButton that makes a new <button> element
+
+    const removeButton = document.createElement('button');
+        removeButton.innerText = 'remove';
+        removeButton.type  = 'button';
+        removeButton.addEventListener('click', (e) => {
+            const entry = removeButton.parentNode;
+            entry.remove();
+        });
+
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+
+    messageForm.reset();
+});
